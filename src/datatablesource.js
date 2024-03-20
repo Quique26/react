d30e1005341cb14726357dd99b6a6a82a956a0ff
Quique26 <luis.nunez@cuidum.com>
2024-03-20 16:47:12 +0100

    react single item page design

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -25,7 +25,7 @@ export const userRows = [
     {
         id: 1,
         username: "Snow",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/1820710/pexels-photo-1820710.jpeg",
         email: "snow@gmail.com",
         status: "active",
         age: 35,
@@ -34,7 +34,7 @@ export const userRows = [
     {
         id: 2,
         username: "Jamie Lannister",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/941623/pexels-photo-941623.jpeg",
         email: "jamie@gmail.com",
         status: "passive",
         age: 42,
@@ -43,7 +43,7 @@ export const userRows = [
     {
         id: 3,
         username: "Lannister",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/941603/pexels-photo-941603.jpeg",
         email: "lannister@gmail.com",
         status: "pending",
         age: 45,
@@ -52,7 +52,7 @@ export const userRows = [
     {
         id: 4,
         username: "Stark",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/1820700/pexels-photo-1820700.jpeg",
         email: "stark@gmail.com",
         status: "active",
         age: 16,
@@ -61,7 +61,7 @@ export const userRows = [
     {
         id: 5,
         username: "Targaryen",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/1820750/pexels-photo-1820750.jpeg",
         email: "targaryen@gmail.com",
         status: "passive",
         age: 22,
@@ -70,7 +70,7 @@ export const userRows = [
     {
         id: 6,
         username: "Melisandre",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/941663/pexels-photo-941663.jpeg",
         email: "melisandre@gmail.com",
         status: "active",
         age: 15,
@@ -88,7 +88,7 @@ export const userRows = [
     {
         id: 8,
         username: "Frances",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/1820780/pexels-photo-1820780.jpeg",
         email: "frances@gmail.com",
         status: "active",
         age: 36,
@@ -97,7 +97,7 @@ export const userRows = [
     {
         id: 9,
         username: "Roxie",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/1820790/pexels-photo-1820790.jpeg",
         email: "roxie@gmail.com",
         status: "pending",
         age: 65,
@@ -106,7 +106,7 @@ export const userRows = [
     {
         id: 10,
         username: "Roxie",
-        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg",
+        img: "https://images.pexels.com/photos/941683/pexels-photo-941683.jpeg",
         email: "10roxie@gmail.com",
         status: "active",
         age: 65,
