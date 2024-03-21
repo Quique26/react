8c4aa77024aa6110d18d78dcdffc813cb90c4444
Quique26 <luis.nunez@cuidum.com>
2024-03-21 10:45:14 +0100

    react form page design

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { productInputs, userInputs } from "./formSource";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
@@ -11,17 +12,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />}/>
-            <Route path="login" element={<Login />}/>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
             <Route path="users">
-              <Route index element={<List />}/>
+              <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />}/>
+              <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
             </Route>
             <Route path="products">
-              <Route index element={<List />}/>
+              <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />}/>
+              <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
             </Route>
           </Route>
         </Routes>
